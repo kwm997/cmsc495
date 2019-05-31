550b9a8d65eb8bc07622dd8316b1c4fa685cc687
kwm997 <50435156+kwm997@users.noreply.github.com>
2019-05-31 02:17:49 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
   <head>
-    <body>
-      <center><h1>Welcome - CMSC495</h1></center>
-    </body>
   </head>
+  <body>
+    <center><h1>Welcome - CMSC495</h1></center>
+  </body>
 </html>
